3a2d114062999a5c187b40b80616098e3286fa2b
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-01-28 16:30:47 +0600

    array basic

---
+++ array.js
@@ -0,0 +1,64 @@
+// declare an array
+/*var fruits = ["apple", "banana", "orange", "jackfruit", "mango"];
+console.log(fruits[3]);
+fruits[2] = "jambura";
+console.log(fruits);*/
+
+// *************************************************
+
+// add or remove elements
+
+/*var touristDestination = ["coxbaxar", "sentmartin", "bandarban"];
+touristDestination.push("rangamati");
+touristDestination.push("sajek" , "potenga");
+touristDestination.pop();
+console.log(touristDestination); // [ 'coxbaxar', 'sentmartin', 'bandarban', 'rangamati', 'sajek' ]*/
+
+// *************************************************
+
+// Checking Array Membership with ‘includes’
+
+/*var books = ["comedy", "love", "programing", "cooking"];
+console.log(books.includes("javascript"));
+
+// if(books.includes("javascript")){
+//    console.log("present");
+// }
+// else{
+//     console.log("not");
+// }
+
+// var elements = books.includes("javascript") ? "present" : "not";
+
+// console.log(elements);*/
+
+// *************************************************
+
+//Checking if it's an Array
+
+// var number = 12;
+// var firstName = "abdul";
+// var books = ["comedy", "love", "programing", "cooking"];
+
+// console.log(Array.isArray(number));
+// console.log(Array.isArray(firstName));
+// console.log(Array.isArray(books));
+
+// if(Array.isArray(number)){
+//     console.log("array");
+// }
+// else{
+//     console.log("not");
+// }
+
+// *************************************************
+// Combining Arrays
+/*
+var books = ["comedy", "love", "programing", "cooking"];
+var fruits = ["apple", "banana", "orange", "jackfruit", "mango"];
+
+var combined = books.concat(fruits);
+console.log(combined);
+console.log(combined.length);*/
+
+// *************************************************
